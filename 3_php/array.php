bb1f58fddb7bc8eec6f3887582cf4cf881acbf19
max1303 <max1303@rambler.ru>
2019-11-18 22:01:30 +0300

    2019.11.18_2

--- 3_php/array.php
+++ 3_php/array.php
@@ -29,15 +29,60 @@ if (mysqli_connect_errno()) {
     exit();
 }
 $query = "SELECT * from `lot1` WHERE user_id = '10'";
-$res = mysqli_query($connect, $query);
+$res = $connect->query($query);
 $i=1;
 if($res){
-	$marr[] = mysqli_fetch_array($res);
+	$marr[] = $res->fetch_array(MYSQLI_ASSOC);
 	
 }
 foreach ($marr as $key => $value) {
 	echo "key ".$key."val ".$value."<br>";
 }
+
+// Функция in_array() осуществляет поиск значения в массиве и имеет следующий
+// синтаксис:
+Ьооl in_array(mixed $value, array $arr [, Ьооl $strict = false ] )
+// Функция ищет в массиве $arr значение $value и возвращает true, если значение
+// найдено, и false - в противном случае 
+$nшnЬers = [0.57, '21.5', 40.52];
+if (in_array(21.5, $nшnЬers)) {
+	echo 'Значение 21.5 найдено';
+else {
+	echo 'Ничего не найдено ';
+}
+
+
+// Аналогично функции in_array() для поиска заданного ключа в массиве можно воспользоваться
+// функцией array_key_exists(), которая имеет следующий синтаксис:
+// Ьооl array_key_exists(mixed $key, array $array)
+// Функция возвращает true, если ключ $key найден в массиве $arr
+$arr = ['first_numЬ' => 1, 'second_numЬ' => 2];
+if(array_key_exists('first_numЬ', $arr){
+	echo 'ОК';
+}
+
+
+
+//	Найти ключ массива по значению позволяет функция array_search(), которая имеет
+//	следующий синтаксис:
+mixed array_search(mixed $value, array $arr[, Ьооl $strict = false])
+//	Функция ищет значение $value в массиве $arr и, если значение найдено, возвращает
+//	соответствующий ключ, в противном случае возвращается false. Если необязательный
+//	параметр $strict принимает значение true, то при сравнении значения
+//	элемента массива с $value будет использоваться оператор эквивалентности ===,
+//	в противном случае будет использован оператор равенства ==. 
+
+$array = [О => 'Ыuе', 1 => 'red', 2 => 'green', 3 => 'red'J;
+$key array_search('green', $array); // $key 2;
+$key array_search( 'red', $array) ; // $key 1;
+
+
+
+
+
+
+
+
 
 ?>
 
